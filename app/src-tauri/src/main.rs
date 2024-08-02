79d77532c0da9b66cf7b0c11cc9fada85e8e8db5
Calbabreaker <calbabreaker@gmail.com>
2024-08-02 13:26:32 +0930

    fix: app main

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -3,21 +3,18 @@
 
 use std::time::Duration;
 
-const RESTART_WAIT_TIME: Duration = Duration::from_secs(3);
-
 fn main() {
     micap_server::setup_log();
     tauri::async_runtime::spawn(async {
-        loop {
-            if let Err(error) = micap_server::start_server().await {
-                log::error!("Server error: {error:?}");
-                if !matches!(std::env::var("RUST_BACKTRACE"), Ok(1)) {
-                    log::error!("  Note: set RUST_BACKTRACE=1 to see the error backtrace")
-                }
-
-                log::error!("Restarting in {RESTART_WAIT_TIME:?}...");
-                tokio::time::sleep(RESTART_WAIT_TIME).await;
+        if let Err(error) = micap_server::start_server().await {
+            log::error!("Server error: {error:?}");
+            if std::env::var("RUST_BACKTRACE") != Ok("1".to_string()) {
+                log::error!(
+                    "Note: set environment variable RUST_BACKTRACE=1 to see the error backtrace"
+                )
             }
+
+            std::process::exit(1);
         }
     });
 
