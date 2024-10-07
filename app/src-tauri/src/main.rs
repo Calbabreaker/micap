e46fa08c02d150e422a50b28dd9d600ee4ff2acc
Calbabreaker <calbabreaker@gmail.com>
2024-10-08 02:15:46 +1030

    feat: system tray

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 fn main() {
     // If LANG not set to en, it shows blank window for some reason
     std::env::set_var("LANG", "en");
@@ -21,7 +23,45 @@ fn main() {
     });
 
     tauri::Builder::default()
-        .setup(|_app| Ok(()))
+        .setup(setup)
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("Error while running tauri application");
+}
+
+fn setup(app: &mut tauri::App) -> Result<(), Box<dyn std::error::Error>> {
+    create_system_tray(app)?;
+
+    let window = app.get_webview_window("main").unwrap();
+    let w = window.clone();
+    window.on_window_event(move |event| {
+        if let tauri::WindowEvent::CloseRequested { api, .. } = event {
+            api.prevent_close();
+            w.hide().unwrap();
+        }
+    });
+
+    Ok(())
+}
+
+fn create_system_tray(app: &tauri::App) -> tauri::Result<tauri::tray::TrayIcon> {
+    let quit_i = tauri::menu::MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+    let show_i = tauri::menu::MenuItem::with_id(app, "show", "Show", true, None::<&str>)?;
+    let menu = tauri::menu::Menu::with_items(app, &[&quit_i, &show_i])?;
+
+    tauri::tray::TrayIconBuilder::new()
+        .icon(app.default_window_icon().unwrap().clone())
+        .menu(&menu)
+        .menu_on_left_click(true)
+        .on_menu_event(|app, event| match event.id.as_ref() {
+            "quit" => {
+                app.exit(0);
+            }
+            "show" => {
+                app.get_webview_window("main").unwrap().show().unwrap();
+            }
+            _ => {
+                log::error!("Unknown menu id {:?}", event.id);
+            }
+        })
+        .build(app)
 }
